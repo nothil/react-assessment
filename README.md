9019d5073f63ee4d51ad4e5a6780f0fad2931db3
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2020-06-14 20:55:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Flexyforce React Assessment
+
+## Star Wars Api
+
+## project setup
+
+* Node
+* webpack
+
+
+
+## Install
+
+### clone the reposatry and install depeendancies.
+### commands: 
+
+``git clone ``
+``npm install``
+
+## Run the App locally
+
+### command:
+
+``npm run serve``
+
+
+
+
+
---
+++ src/App.js
@@ -0,0 +1,65 @@
+ 
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const STAR_WARS_FILMS = gql`
+  {
+    allFilms(first: 6, orderBy: releaseDate_ASC) {
+      episodeId
+      title
+      releaseDate
+      director
+    }
+  }
+`;
+
+export default function App() {
+  const { loading: loadingFilms, error: errorFilm, data: dataFilms } = useQuery(
+    STAR_WARS_FILMS
+  );
+
+  if (loadingFilms) return <p>Loading...</p>;
+  if (errorFilm) return <p>There's an error: {errorFilm.message}</p>;
+
+  console.dir(dataFilms);
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '18px',
+    cursor: 'pointer'
+  };
+
+  const main_box = {
+    border: '1px solid #eee',
+    boxShadow: '0 2px 2px #ccc',
+    width: '400px',
+    padding: '20px'
+    
+  }
+
+  
+
+  const films = dataFilms.allFilms.map(film => (
+    
+    <div style={main_box}>
+      <p key={film.episodeId}></p>
+
+      <p> <b>Title:{" "} </b>{film.title}</p>
+      <p> <b>Director:{" "}</b> {film.director}</p>
+      <p> <b>Release Date:</b>{" "}{film.releaseDate.substr(0, 4)}</p>
+    </div>
+    
+  ));
+  return (
+    <Fragment>
+
+      
+      <h1>Star Wars Films</h1>
+      {films}
+      
+    </Fragment>
+  );
+}
